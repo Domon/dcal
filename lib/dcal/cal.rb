adff281cca5f1c9c2625e86ccaf86825d114b3f1
Chun-wei Kuo <Dendoh@gmail.com>
2012-12-09 20:44:18 +0800

    Complete basic functionalities

--- lib/dcal/cal.rb
+++ lib/dcal/cal.rb
@@ -12,25 +12,71 @@ module DCal
 
     ABBR_DAYNAMES = %w(Su Mo Tu We Th Fr Sa)
 
+    COMMON_YEAR_DAYS_IN_MONTH = [
+      nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    ]
+
     def initialize(month, year)
       @month = month.to_i
       @year  = year.to_i
     end
 
     def output
-      title.center(20) + "\n" +
-        header         + "\n"
-      # + weeks
+      title    + "\n" +
+        header + "\n" +
+        weeks  + "\n"
     end
 
     private
 
     def title
-      "#{MONTHNAMES[month]} #{year}"
+      "#{MONTHNAMES[month]} #{year}".center(20)
     end
 
     def header
       ABBR_DAYNAMES.join(" ")
+    end
+
+    def weeks
+      weeks = days.each_slice(7).to_a
+      weeks.map { |w| w.join(" ") }.join("\n")
+    end
+
+    def days
+      days = Array.new(42, "  ")
+      (1..days_in_month(month)).each do |day|
+        days[weekday_of_first_day + day - 1] = day.to_s.rjust(2)
+      end
+      days
+    end
+
+    def weekday_of_first_day
+      # 1970-01-01 is Thursday (4).
+      (days_since_1970 + 4) % 7
+    end
+
+    def days_since_1970
+      (year - 1970) * 365 + leap_years_since_1970 + passed_days_in_year
+    end
+
+    def leap_years_since_1970
+      return 0 if year == 1970
+      (1970...year).select { |y| in_leap_year?(y) }.size
+    end
+
+    def passed_days_in_year
+      return 0 if month == 1
+      (1...month).reduce(0) { |sum, m| sum + days_in_month(m) }
+    end
+
+    def days_in_month(month)
+      return 29 if month == 2 && in_leap_year?(year)
+      COMMON_YEAR_DAYS_IN_MONTH[month]
+    end
+
+    def in_leap_year?(year)
+      # ref: http://en.wikipedia.org/wiki/Leap_year#Algorithm
+      year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)
     end
   end
 end
--- test/dcal/test_cal_output.rb
+++ test/dcal/test_cal_output.rb
@@ -2,15 +2,46 @@ require 'test/unit'
 require 'dcal'
 
 class TestCalOutput < Test::Unit::TestCase
-  def setup
-    @cal = DCal::Cal.new 12, 2012
-  end
-
   def test_output
-    sample_output = <<-OUTPUT
+    output_1_1970 = <<-OUTPUT
+    January 1970    
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+                    
+OUTPUT
+
+    output_8_1998 = <<-OUTPUT
+    August 1998     
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31               
+OUTPUT
+
+    output_12_2012 = <<-OUTPUT
    December 2012    
 Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31               
 OUTPUT
-    assert_equal sample_output, @cal.output
+
+    cal_1_1970  = DCal::Cal.new( 1, 1970)
+    cal_8_1998  = DCal::Cal.new( 8, 1998)
+    cal_12_2012 = DCal::Cal.new(12, 2012)
+
+    assert_equal output_1_1970,  cal_1_1970.output
+    assert_equal output_8_1998,  cal_8_1998.output
+    assert_equal output_12_2012, cal_12_2012.output
   end
 end
